0c135fddcc192bb71cd0b6be9345a4b91d821089
Ronit raj <ronitraj42041@gmail.com>
2018-03-16 11:43:37 +0530

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
  * @author ronit
  */
 public class Minesweeper extends Application {  
-    int noOfRows; //these are the default values 
+    int noOfRows;
     int noOfCols;
     int mines;
     int flagsUsed;
